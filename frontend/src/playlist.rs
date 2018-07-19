5f1549d4e750513d7ac52612f988a04ebb11ef40
Sven-Hendrik Haase <svenstaro@gmail.com>
2018-07-19 09:36:22 +0200

    Something is broken :(

--- frontend/src/playlist.rs
+++ frontend/src/playlist.rs
@@ -4,10 +4,15 @@ use playlist_entry::PlaylistEntry;
 
 pub struct Playlist {
     entries: Vec<String>,
+    current_entry: String,
 }
 
 pub enum Msg {
-    PlaylistEntryClicked,
+    AddEntry,
+    UpdateCurrentEntry(String),
+    EntryClicked(usize),
+    ClearPlaylist,
+    NoOp,
 }
 
 #[derive(PartialEq, Clone)]
@@ -27,12 +32,17 @@ impl Component for Playlist {
     fn create(props: Self::Properties, _: ComponentLink<Self>) -> Self {
         Playlist {
             entries: vec![],
+            current_entry: "".to_string(),
         }
     }
 
     fn update(&mut self, msg: Self::Message) -> ShouldRender {
         match msg {
-            Msg::PlaylistEntryClicked => self.entries.clear()
+            Msg::AddEntry => self.entries.push(self.current_entry.clone()),
+            Msg::UpdateCurrentEntry(val) => self.current_entry = val,
+            Msg::EntryClicked(val) => println!("{}", val),
+            Msg::ClearPlaylist => self.entries.clear(),
+            Msg::NoOp => {},
         }
         true
     }
@@ -40,11 +50,18 @@ impl Component for Playlist {
 
 impl Renderable<Playlist> for Playlist {
     fn view(&self) -> Html<Self> {
-        let playlist_entry = |x| html! {
-            <PlaylistEntry: title=x, onsignal=|_| Msg::PlaylistEntryClicked, />
+        let playlist_entry = |(i, val): (usize, &String)| html! {
+            <PlaylistEntry: title=val, onsignal=|_| Msg::EntryClicked(i.clone()), />
         };
         html! {
-            { for self.entries.iter().map(playlist_entry) }
+            { for self.entries.iter().enumerate().map(playlist_entry) }
+            <input value=&self.current_entry,
+                   type="text",
+                   oninput=|e| Msg::UpdateCurrentEntry(e.value),
+                   onkeypress=|e| {
+                       if e.key() == "Enter" { Msg::AddEntry } else { Msg::NoOp }
+                   },
+            />
         }
     }
 }
